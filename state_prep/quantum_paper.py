e0c829521a156df4c9722ecb27604f1651d4aeaa
Evan Dobbs <edjunk13@hotmail.com>
2025-02-02 01:27:52 +0200

    debugged version of quantum_paper.py

--- state_prep/quantum_paper.py
+++ state_prep/quantum_paper.py
@@ -1,4 +1,6 @@
 from classiq import *
+import numpy as np
+
 
 def generate_gaussian(num_qubits):
   resolution = num_qubits
@@ -11,9 +13,24 @@ def generate_gaussian(num_qubits):
 
 
 @qfunc
+def hadamard_something(x: QArray, index: CInt):
+  hadamard_transform(x[index])
+
+
+@qfunc
+def add_one(x: QNum):
+  x+=1
+
+
+@qfunc
+def controlled_add(x: QArray, index: CInt):
+   control(ctrl=(x[index] == 1), stmt_block=(lambda: add_one(x[0:index])))
+   
+
+@qfunc
 def main(x: Output[QNum]):
   num_qubits = 8
-  allocate(x, num_qubits)
+  allocate(num_qubits, x)
   mu_hat = 0
   sigma_hat = 1
   probs = generate_gaussian(8)
@@ -23,26 +40,34 @@ def main(x: Output[QNum]):
   m = num_qubits - n_1 + 1
 
   bigN = 2**num_qubits
-  sigma = (bigN^2 / l^2) * sigma_hat
+  sigma = (bigN**2 / l**2) * sigma_hat
   mu = (bigN / l) * mu_hat
   big_sum = 0
   for k in range(2, m+1):
-    big_sum += 4^(m-k)
+    big_sum += 4**(m-k)
   iters = []
-  iters[0] = (sigma - (4^(num_qubits-1)-1)/12 - (num_iters/4)*(big_sum)) / 4^(num_qubits-1)
+  iters.append((sigma - (4**(num_qubits-1)-1)/12 - (num_iters/4)*(big_sum)) / 4**(num_qubits-1))
   for i in range(1, m+1):
-    iters[i] = num_iters
+    iters.append(num_iters)
 
-  for i in range(1, m+1): # loop IV: there are i qubits in the Gaussian state.
+  for i in range(1, m): # loop IV: there are i qubits in the Gaussian state.
     for j in range(1, iters[i]+1):
       # add a state in |+> to the register
-      hadamard_transform(x[i]) # TODO: apply hadamard to all of x at the beginning of the circuit.
+      hadamard_something(x, i) # TODO: apply hadamard to all of x at the beginning of the circuit instead.
       # add the register and 1 controlled on the |+> qubit
-      control(ctrl=(x[i] == 1), stmt_block=(lambda: ))
+      controlled_add(x, i)
       # apply a Hadamard to the |+> qubit
+      hadamard_something(x, i)
       # measure the (former) |+> qubit
       # if measured 1, try again. Else (move on or do next iteration?)??
-      
+      #if x[i] == 1:
+      #  raise ValueError("measured a 1, restart the procedure")
+  temp_sum = 0
+  for k in range(1, num_qubits+1):
+    temp_sum += 2**(num_qubits - k) * iters[k]
+  alpha = mu - (2**(num_qubits-1)-1 + temp_sum) / 2
+  x += alpha
+
 
 
 
